609a0058503b9e620daf2b14ff8366ff1f8ca3e2
Douglas Mayle <douglas@mayle.org>
2014-03-10 13:03:29 -0700

    Use set command if necessary

--- lib/puppet/provider/plist/plistbuddy.rb
+++ lib/puppet/provider/plist/plistbuddy.rb
@@ -17,28 +17,30 @@ Puppet::Type.type(:plist).provide :plistbuddy, :parent => Puppet::Provider do
         keys = @resource.keys
         value_type = @resource.value_type
 
+	cmd = keypresent? ? "Set" : "Add"
+
         if value_type == :array
 
           # Add the array entry
-          buddycmd = "Add :%s %s %s" % [ keys.join(':'), value_type, @resource[:value] ]
+          buddycmd = "%s :%s %s %s" % [ cmd, keys.join(':'), value_type, @resource[:value] ]
 
           # Add the elements
           @resource[:value].each do |value|
             plistbuddy(file_path, '-c', buddycmd)
-            buddycmd = "Add :%s:0 %s %s" % [ keys.join(':'), 'string', value ]
+            buddycmd = "%s :%s:0 %s %s" % [ cmd, keys.join(':'), 'string', value ]
           end
         elsif value_type == :date # Example of a date that PlistBuddy will accept Mon Jan 01 00:00:00 EST 4001
           native_date = Date.parse(@resource[:value])
           # Note that PlistBuddy will only accept certain timezone formats like 'EST' or 'GMT' but not other valid
           # timezones like 'PST'. So the compromise is that times must be in UTC
-          buddycmd = "Add :%s %s %s" % [ keys.join(':'), value_type,  native_date.strftime('%a %b %d %H:%M:%S %Y')]
+          buddycmd = "%s :%s %s %s" % [ cmd, keys.join(':'), value_type,  native_date.strftime('%a %b %d %H:%M:%S %Y')]
         else
-          buddycmd = "Add :%s %s %s" % [ keys.join(':'), value_type, @resource[:value] ]
+          buddycmd = "%s :%s %s %s" % [ cmd, keys.join(':'), value_type, @resource[:value] ]
         end
 
         plistbuddy(file_path, '-c', buddycmd)
 
-      rescue Exception => e
+      rescue Exception
         false
       end
   end
@@ -50,7 +52,24 @@ Puppet::Type.type(:plist).provide :plistbuddy, :parent => Puppet::Provider do
 
       buddycmd = "Delete :%s" % keys.join(':')
       plistbuddy(file_path, '-c', buddycmd)
-    rescue Exception => e
+    rescue Exception
+      false
+    end
+  end
+
+  def keypresent?
+
+    begin
+      file_path = @resource.filename
+      keys = @resource.keys
+
+      buddycmd = "Print :%s" % keys.join(':')
+      plistbuddy(file_path, '-c', buddycmd).strip
+
+      true
+
+    rescue Exception
+      # A bad return value from plistbuddy indicates that the key does not exist.
       false
     end
   end
@@ -78,7 +97,7 @@ Puppet::Type.type(:plist).provide :plistbuddy, :parent => Puppet::Provider do
           @resource[:value].to_s == buddyvalue
       end
 
-    rescue Exception => e
+    rescue Exception
       # A bad return value from plistbuddy indicates that the key does not exist.
       false
     end
